7ca1834e125186cf240b87ca877769fa3e0ac99f
cebrailkeskindag <cebrailkeskindag1549@gmail.com>
2023-11-04 23:23:29 +0300

    tr ve en dil desteği yüklendi.

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tasarim/renkler.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class Anasayfa extends StatefulWidget {
   const Anasayfa({super.key});
@@ -11,12 +12,20 @@ class Anasayfa extends StatefulWidget {
 class _AnasayfaState extends State<Anasayfa> {
   @override
   Widget build(BuildContext context) {
+    var ekranBilgisi = MediaQuery.of(context);
+    final double ekranYuksekligi = ekranBilgisi.size.height;
+    final double ekranGenisligi = ekranBilgisi.size.width;
+
+    var d = AppLocalizations.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "Pizza",
-          style:
-              TextStyle(color: yaziRenk1, fontSize: 22, fontFamily: "Roboto"),
+          style: TextStyle(
+              color: yaziRenk1,
+              fontSize: ekranGenisligi / 18,
+              fontFamily: "Roboto"),
         ),
         backgroundColor: anaRenk,
         centerTitle: true,
@@ -24,72 +33,112 @@ class _AnasayfaState extends State<Anasayfa> {
       body: Column(
         children: [
           Padding(
-            padding: EdgeInsets.only(top: 16.0),
-            child: Text("Beef Cheese",
+            padding: EdgeInsets.only(top: ekranGenisligi / 43),
+            child: Text(d!.pizzaBaslik,
                 style: TextStyle(
-                    fontSize: 36, color: anaRenk, fontWeight: FontWeight.bold)),
+                    fontSize: ekranGenisligi / 11,
+                    color: anaRenk,
+                    fontWeight: FontWeight.bold)),
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 16.0),
+            padding: EdgeInsets.only(top: ekranGenisligi / 43),
             child: Image.asset("resimler/pizza.png"),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 16),
-            child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                TextButton(onPressed: (){},
-                child: Text("Cheese",style: TextStyle(color: yaziRenk1),),
-                style: TextButton.styleFrom(backgroundColor: anaRenk),
+                Chip(icerik: d!.peynirYazi),
+                Chip(icerik: d!.sucukYazi),
+                Chip(icerik: d!.zeytinYazi),
+                Chip(icerik: d!.biberYazi),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(ekranGenisligi / 35),
+            child: Column(
+              children: [
+                Text(
+                  d!.teslimatSure,
+                  style: TextStyle(
+                      fontSize: ekranGenisligi / 20,
+                      color: yaziRenk2,
+                      fontWeight: FontWeight.bold),
                 ),
-                TextButton(onPressed: (){},
-                child: Text("Sausage",style: TextStyle(color: yaziRenk1),),
-                style: TextButton.styleFrom(backgroundColor: anaRenk),
+                Padding(
+                  padding: EdgeInsets.all(ekranGenisligi / 35),
+                  child: Text(
+                    d!.teslimatBaslik,
+                    style: TextStyle(
+                        fontSize: ekranGenisligi / 20,
+                        color: anaRenk,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
-                TextButton(onPressed: (){},
-                child: Text("Olive",style: TextStyle(color: yaziRenk1),),
-                style: TextButton.styleFrom(backgroundColor: anaRenk),
-                ),
-                TextButton(onPressed: (){},
-                child: Text("Pepper",style: TextStyle(color: yaziRenk1),),
-                style: TextButton.styleFrom(backgroundColor: anaRenk),
+                Text(
+                  d!.pizzaAciklama,
+                  style: TextStyle(
+                    fontSize: ekranGenisligi / 19,
+                    color: yaziRenk2,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
               ],
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-              Text("20 Min", style: TextStyle(fontSize: 22, color: yaziRenk2, fontWeight: FontWeight.bold),),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text("Delevery", style: TextStyle(fontSize: 22, color: anaRenk, fontWeight: FontWeight.bold),),
-              ),
-              Text("Meat lover, get ready to meet your pizza!",
-            style: TextStyle(fontSize: 22, color: yaziRenk2,), textAlign: TextAlign.center,),
-            ],
-            ),
-           ),
-           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
               children: [
-                Text("\$5.98", style: TextStyle(fontSize: 44, color: anaRenk, fontWeight: FontWeight.bold),),
+                Text(
+                  d!.fiyat,
+                  style: TextStyle(
+                      fontSize: ekranGenisligi / 10,
+                      color: anaRenk,
+                      fontWeight: FontWeight.bold),
+                ),
                 const Spacer(),
-                SizedBox(width: 200, height: 50,
-                child: TextButton(onPressed: () {},
-                child: Text("AD TO CART", style: TextStyle(fontSize: 18, color: yaziRenk1),),
-                style: TextButton.styleFrom(
-                  backgroundColor: anaRenk,
-                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
-                ),
-                ),
+                SizedBox(
+                  width: ekranGenisligi / 2,
+                  height: ekranYuksekligi / 17,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      d!.butonYazi,
+                      style: TextStyle(
+                          fontSize: ekranGenisligi / 20, color: yaziRenk1),
+                    ),
+                    style: TextButton.styleFrom(
+                      backgroundColor: anaRenk,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5))),
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class Chip extends StatelessWidget {
+  String icerik;
+  Chip({required this.icerik});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      child: Text(
+        icerik,
+        style: TextStyle(color: yaziRenk1),
+      ),
+      style: TextButton.styleFrom(backgroundColor: anaRenk),
     );
   }
 }
